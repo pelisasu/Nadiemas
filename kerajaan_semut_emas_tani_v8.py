376e6f62744bbe8f868f92efe38e989c50364e76
pelisasu <pelisasaja5@gmail.com>
2026-09-14 04:42:30 +0700

    Update kerajaan_semut_emas_tani_v8.py

--- kerajaan_semut_emas_tani_v8.py
+++ kerajaan_semut_emas_tani_v8.py
@@ -12,7 +12,7 @@ from datetime import datetime, timezone, timedelta
 import numpy as np
 
 CONFIG={
-    "OFFSET": -7.75,
+    "OFFSET": -2.41,
     "DNA_FILE": ".dna_tani_v8.json",
     "MEMORY_FILE": ".memory_tani_v8.json",
     "LAST_FILE": ".last_tani_v8.json",
